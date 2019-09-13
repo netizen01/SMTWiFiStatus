71a5453fe4429911ed30e0b6190f87913345f30f
joelstewart <joel@codeinversion.com>
2019-09-13 10:32:24 -0500

    Add Swift PM support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SMTWiFiStatus",
+    products: [
+        .library(name: "SMTWiFiStatus", targets: ["SMTWiFiStatus"]),
+    ],
+    targets: [
+        .target(
+        	name: "SMTWiFiStatus",
+        	path: "SMTWiFiStatus/Classes",
+            publicHeadersPath: "."
+        )
+    ]
+)
